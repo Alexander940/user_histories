a3813298d20c0a050022a31ccd47b48d364a0a05
Alexander Echeverry Ramirez <alexanderecheverry@outlook.es>
2022-10-24 11:48:37 -0500

    first commit

--- components/ParticipantList.js
+++ components/ParticipantList.js
@@ -3,7 +3,9 @@ import styles from '../styles/ParticipantList.module.css'
 function ParticipantList(props) {
     return (
         <section>
-            <ul className={styles.participantList}>
+            <ul 
+                className={styles.participantList}
+            >
                 {props.children}
             </ul>
         </section>
